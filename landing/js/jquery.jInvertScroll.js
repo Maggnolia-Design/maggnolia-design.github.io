295f59113084fe47dc3b8ad58b7ba4d75ebef7fe
MistaFish31 <zhang.ma@husky.neu.edu>
2016-05-17 00:09:14 -0400

    horizontal parallax
    with fish; jinvertScroll.

---
+++ landing/js/jquery.jInvertScroll.js
@@ -0,0 +1,71 @@
+(function($) {
+    $.jInvertScroll = function(sel, options) {
+        var defaults = {
+            width: 'auto',		    // The horizontal container width
+            height: 'auto',		    // How far the user can scroll down (shorter distance = faster scrolling)
+            onScroll: function(percent) {  // Callback fired when the user scrolls down, the percentage of how far the user has scrolled down gets passed as parameter (format: 0.xxxx - 1.0000)
+                // do whatever you like
+            }
+        };
+        
+        var config = $.extend(defaults, options);
+        
+        if(typeof sel === 'Object' && sel.length > 0) {
+            return;
+        }
+        
+        var elements = [];
+        var longest = 0;
+        
+        // Extract all selected elements from dom and save them into an array
+        $.each(sel, function(i, val) {
+            $(val).each(function(e) {
+                var tmp = {
+                    width: $(this).width(),
+                    height: $(this).height(),
+                    el: $(this)
+                }
+                
+                elements.push(tmp);
+                
+                if(longest < tmp.width) {
+                    longest = tmp.width;
+                }
+            });
+        });
+        
+        // Use the longest elements width + height if set to auto
+        if(config.width == 'auto') {
+            config.width = longest;
+        }
+        
+        if(config.height == 'auto') {
+            config.height = longest;
+        }
+        
+        // Set the body to the selected height
+        $('body').css('height', config.height+'px');
+        
+        // Listen for the actual scroll event
+        $(window).on('scroll resize', function(e) {
+            var currY = $(this).scrollTop();
+            var totalHeight = $(document).height();
+            var winHeight = $(this).height();
+            var winWidth = $(this).width();
+            
+            // Current percentual position
+            var percent = (currY / (totalHeight - winHeight)).toFixed(4);
+            
+            // Call the onScroll callback
+            if(typeof config.onScroll === 'function') {
+                config.onScroll.call(this, percent);
+            }
+            
+            // do the position calculation for each element
+            $.each(elements, function(i, el) {
+                var pos = Math.floor((el.width - winWidth) * percent) * -1;
+                el.el.css('right', pos);
+            });
+        });
+    };
+}(jQuery));
