8fabf517862906c0d0088f1b824925ecd6bdfa11
MistaFish31 <zhang.ma@husky.neu.edu>
2016-09-28 17:28:13 -0400

    finished-jquery-nav
    all the pages are condensed into index.html; the nav works on all pages

--- js/script1.js
+++ js/script1.js
@@ -1,17 +1,54 @@
 $(document).ready(function(){
 
-   $("#portfolio-btn").click(function(event){
+    $("#home-btn").click(function(event){
      //Your actions here
      $(".pages").hide();
-     $("#portfolio-page").show();
-     $(".nav-link").removeClass("active");
-     $("#portfolio-btn").addClass("active")
+     $("#home-page").show();
+   });
+
+    $("#og-home-button").click(function(event){
+     //Your actions here
+     $(".pages").hide();
+     $("#home-page").show();
+   });
+
+    $("#about-btn").click(function(event){
+     //Your actions here
+     $(".pages").hide();
+     $("#about-page").show();
    });
 
    $("#contact-btn").click(function(event){
      //Your actions here
      $(".pages").hide();
      $("#contact-page").show();
+   });
+
+   // PORTFOLIO SECTION
+   $("#portfolio-btn").click(function(event){
+     //Your actions here
+     $(".pages").hide();
+     $("#portfolio-page").show();
+     // $(".nav-link").removeClass("active");
+     // $("#portfolio-btn").addClass("active")
+   });
+
+   $("#classwork-btn").click(function(event){
+     //Your actions here
+     $(".pages").hide();
+     $("#classwork-page").show();
+   });
+
+   $("#student-btn").click(function(event){
+     //Your actions here
+     $(".pages").hide();
+     $("#student-page").show();
+   });
+
+   $("#chinese-btn").click(function(event){
+     //Your actions here
+     $(".pages").hide();
+     $("#chinese-page").show();
    });
 
  });
