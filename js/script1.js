9bfbbfb2bcba8f962a95a346765ca4251dac32c1
MistaFish31 <zhang.ma@husky.neu.edu>
2016-10-04 22:34:05 -0400

    active tabs
    fixed highlighted color on active tabs

--- js/script1.js
+++ js/script1.js
@@ -4,6 +4,8 @@ $(document).ready(function(){
      //Your actions here
      $(".pages").hide();
      $("#home-page").show();
+     $("li").removeClass("active");
+     $("#home-btn").addClass("active");
    });
 
     $("#og-home-button").click(function(event){
@@ -16,39 +18,49 @@ $(document).ready(function(){
      //Your actions here
      $(".pages").hide();
      $("#about-page").show();
+     $("li").removeClass("active");
+     $("#about-btn").addClass("active");
    });
 
    $("#contact-btn").click(function(event){
      //Your actions here
      $(".pages").hide();
      $("#contact-page").show();
+     $("li").removeClass("active");
+     $("#contact-btn").addClass("active");
    });
 
-   // PORTFOLIO SECTION
-   $("#portfolio-btn").click(function(event){
-     //Your actions here
-     $(".pages").hide();
-     $("#portfolio-page").show();
-     // $(".nav-link").removeClass("active");
-     // $("#portfolio-btn").addClass("active")
-   });
+   // // PORTFOLIO SECTION
+   // $("#portfolio-btn").click(function(event){
+   //  //Your actions here
+   //  $(".pages").hide();
+   //  $("#portfolio-page").show();
+   //  $("li").removeClass("active");
+   //  $("#portfolio-btn").addClass("active");
+   // });
 
    $("#classwork-btn").click(function(event){
      //Your actions here
      $(".pages").hide();
      $("#classwork-page").show();
+     $("li").removeClass("active");
+     $("#portfolio-btn").addClass("active");
    });
 
    $("#student-btn").click(function(event){
      //Your actions here
      $(".pages").hide();
      $("#student-page").show();
+     $("li").removeClass("active");
+     $("#portfolio-btn").addClass("active");
    });
 
    $("#chinese-btn").click(function(event){
      //Your actions here
      $(".pages").hide();
      $("#chinese-page").show();
+     $("li").removeClass("active");
+     $("#portfolio-btn").addClass("active");
    });
 
  });
