c2366ce3bf4c098fe7d5ac9a4f987680a20edb9d
Maggie Zhang <maggie.zhang.us@gmail.com>
2018-04-15 20:33:42 -0400

    - to _

--- piece_pages.js
+++ piece_pages.js
@@ -1,1 +1,11 @@
-$("????? .image-page").css({'top':($(".piece-page").height()+'px'})
+$( ".image_page" ).each(function() {
+	var id = this.id;
+	var $match = $(.piece_page)
+  console.log( index + ": " + $( this ).text() );
+});
+
+
+
+var image_page_id = $(this).attr('id');
+
+$("????? .image_page").css({'top':($(".piece_page").height()+'px'})
